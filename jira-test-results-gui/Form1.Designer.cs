b68234a9cab01d1888422d35a630390f2a3b1a85
edgewords <info@edgewords.co.uk>
2018-03-31 17:26:58 +0100

    Add project files.

---
+++ jira-test-results-gui/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace jira_test_results_gui
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.textURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textNunit = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textOutput
+            // 
+            this.textOutput.Location = new System.Drawing.Point(12, 229);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.ReadOnly = true;
+            this.textOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textOutput.Size = new System.Drawing.Size(617, 209);
+            this.textOutput.TabIndex = 0;
+            // 
+            // textURL
+            // 
+            this.textURL.Location = new System.Drawing.Point(171, 31);
+            this.textURL.Name = "textURL";
+            this.textURL.Size = new System.Drawing.Size(353, 22);
+            this.textURL.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "JIRA URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "JIRA User Name";
+            // 
+            // textUser
+            // 
+            this.textUser.Location = new System.Drawing.Point(171, 66);
+            this.textUser.Name = "textUser";
+            this.textUser.Size = new System.Drawing.Size(353, 22);
+            this.textUser.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "JIRA Password";
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(171, 107);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.PasswordChar = '*';
+            this.textPassword.Size = new System.Drawing.Size(353, 22);
+            this.textPassword.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "NUnit Test Results File";
+            // 
+            // textNunit
+            // 
+            this.textNunit.Location = new System.Drawing.Point(171, 150);
+            this.textNunit.Name = "textNunit";
+            this.textNunit.Size = new System.Drawing.Size(353, 22);
+            this.textNunit.TabIndex = 4;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(530, 150);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(108, 23);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "NUnit XML (*.xml)|*.xml|All Files (*.*)|*.*";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Process Results";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textNunit);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.textURL);
+            this.Controls.Add(this.textOutput);
+            this.Name = "MainForm";
+            this.Text = "Nunit to JIRA Results Updater";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.TextBox textURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textNunit;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ jira-test-results-gui/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Atlassian.Jira;
+using System.IO;
+using System.Xml;
+
+namespace jira_test_results_gui
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textNunit.Text = this.openFileDialog1.FileName;
+            }
+
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.url = this.textURL.Text;
+            Properties.Settings.Default.user = this.textUser.Text;
+            Properties.Settings.Default.pwd = this.textPassword.Text;
+            Properties.Settings.Default.nunit = this.textNunit.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.textURL.Text = Properties.Settings.Default.url;
+            this.textUser.Text = Properties.Settings.Default.user;
+            this.textPassword.Text = Properties.Settings.Default.pwd;
+            this.textNunit.Text = Properties.Settings.Default.nunit;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            string url = this.textURL.Text;
+            string username = this.textUser.Text;
+            string password = this.textPassword.Text;
+            string xmlPath = this.textNunit.Text;
+
+            this.textOutput.Clear();
+            this.textOutput.AppendText("Starting Processing...\r\n");
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(xmlPath);
+                // get all the xml nodes for the Features
+                XmlNodeList testSuitesName = xmlDoc.GetElementsByTagName("test-suite");
+
+                for (int i = 0; i < testSuitesName.Count; i++)
+                {
+                    //Features have an xml attribute of 'TestFixture'
+                    if (testSuitesName[i].Attributes["type"].Value == "TestFixture")
+                    {
+                        string featureName = testSuitesName[i].Attributes["name"].Value.Replace("Feature", "").Replace("_", "-");
+
+                        try
+                        {
+                            //set the Jira 'Test Status' field value
+                            var jira = Jira.CreateRestClient(url, username, password);
+                            var issue = jira.Issues.GetIssueAsync(featureName).Result;
+                            issue["Test Status"] = testSuitesName[i].Attributes["result"].Value;
+                            issue.SaveChanges();
+                            this.textOutput.AppendText("JIRA Issue: " + featureName + " Test Status Updated to: " + testSuitesName[i].Attributes["result"].Value + "\r\n");
+                        }
+                        catch (Exception err)
+                        {
+                            this.textOutput.AppendText(err.Message + " Error with issue: " + featureName + "\r\n");
+                        }
+                    }
+                }
+            this.textOutput.AppendText("Finished!\r\n");
+
+        }
+    }
+
+}
